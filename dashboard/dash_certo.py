515188e3e9f32c5de9278a9e85245b9b40fdd1c6
Tarcila Landin <tarcilalandin.tce@gmail.com>
2024-07-17 16:30:36 -0300

    Ajustes para streamlit

--- dashboard/dash_certo.py
+++ dashboard/dash_certo.py
@@ -1,18 +1,20 @@
 import streamlit as st
 import duckdb
 import pandas as pd
+import os
 
 # Função para carregar dados do arquivo Parquet
 def load_data():
     con = duckdb.connect()
-    df = con.execute("SELECT * FROM 'data\measurements_summary.parquet'").df()
+    parquet_file_path = os.path.join('data', 'measurements_summary.parquet')
+    df = con.execute(f"SELECT * FROM '{parquet_file_path}'").df()
     con.close()
     return df
- 
+
 # Função principal para criar o dashboard
 def main():
     st.title("Weather Station Summary")
-    st.write("This dashboard shows the summary of weather")
+    st.write("This dashboard shows the summary of weather data from various weather stations.")
     
     # Carregar os dados
     data = load_data()  # Chama a função para executar a consulta
